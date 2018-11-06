940b98b27cc943c1f4600ee0048d25945eb1326a
Boppee <44766060+Boppee@users.noreply.github.com>
2018-11-06 10:10:48 +0100

    Adding to login system

--- Backend/php/class/login.php
+++ Backend/php/class/login.php
@@ -7,17 +7,31 @@ class login {
   private $username;
   private $email;
   private $iv;
+  private $emailCode;
   private $enc;
   private $connection;
 
   function __construct($pwd, $uid){
+
+    session_start();
+
     $this->enc = new encoder("private");
     $this->connection = new connect();
 
     $this->setUsername($uid);
     $this->setPassword($pwd);
 
-    $this->grabUserData();
+    $userData = $this->grabUserData();
+
+    if ($this->Username == $userData["uid"] && password_verify($this->password, $userData["pwd"])) {
+      $this->setIV($userData["iv"]);
+      $email = $this->enc-decode($userData["email"], $this->iv);
+      $this->setEmail($email);
+
+      $this->sendVerifyEmail();
+      $_SESSION["loginAttempt"] = array('userdata' => $userData, $emailCode);
+
+    }
   }
 
   public function setUsername($uid){
@@ -26,9 +40,21 @@ class login {
   public function setPassword($pwd){
     $this->password = $pwd;
   }
-  public function grabUserData(){
+  public function setEmail($email){
+    $this->email = $email;
+  }
+  public function setIV($iv){
+    $this->iv = $iv;
+  }
+  public function grabuserData(){
     $this->connection->newConnectionPre("FetchFromAccounts");
 
+    $sth = $this->connection->prepare("SELECT * FROM `accounts` WHERE `uid` = :uid");
+    $sth->bindParam(':uid', $this->uid);
+    $sth->execute();
+    return $sth->fetch(PDO::FETCH_LAZY);
+  }
+  public function sendVerifyEmail(){
 
   }
 }
