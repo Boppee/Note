0e33de3a6781e54c13ca648da496ff9d38924350
mrboppe <33065458+mrboppe@users.noreply.github.com>
2018-11-13 13:07:23 +0100

    header

--- Backend/pages/pageparts/header.php
+++ Backend/pages/pageparts/header.php
@@ -1,16 +1,24 @@
 <script type="text/javascript">
-  var test = <?php echo json_encode($_SESSION["perms"]["pages"]); ?>;
-  test.forEach(function (value, index) {
+  var pageArray = <?php echo json_encode($_SESSION["perms"]["pages"]); ?>;
+  var navListLenght = 100/pageArray.length;
+
+  pageArray.forEach(function (value, index) {
     $(document).ready(function () {
       $("#navItems").append("<li id='nav"+value+"' class='navItem'><a href='?page="+value+"'>"+value+"</a></li>");
+      var temp = "nav"+value;
+      $("#"+temp).width(navListLenght+"%");
     });
   });
-  var link = "?page="+test[0];
+
+  var link = "?page="+pageArray[0];
   $("#logoLink").attr("href", link);
 </script>
 <header>
-  <div class="inner">
-    <a id="logoLink" href="?page="><img src="" alt="<?php echo $company->companyName; ?>"></a>
+  <div id="headContainer">
+
+    <div id="headLogo">
+      <a id="logoLink" href="?page="><?php echo $company->companyName; ?></a>
+    </div>
 
     <nav>
       <ul id="navItems">
@@ -20,3 +28,4 @@
 
   </div>
 </header>
+<link rel="stylesheet" href="css/pageparts/header.css">
--- Backend/php/class/loader.php
+++ Backend/php/class/loader.php
@@ -20,17 +20,7 @@ class pageLoader {
       $this->goToPage("?page=login");
     }
 
-    if (!isset($_SESSION["signedIn"])) {
-      $_SESSION["perms"]["pages"] = array("login", "cookies");
-      $_SESSION["perms"]["perms"] = array("login");
-      if (!in_array($this->page, $_SESSION["perms"]["pages"])) {
-        $this->goToPage("?page=".$_SESSION["perms"]["pages"][0]);
-      }
-    }else if (isset($_SESSION["signedIn"])){
-      if (!in_array($this->page, $_SESSION)) {
-        $this->goToPage("?page=".$_SESSION["perms"]["pages"][0]);
-      }
-    }
+    //$this->pageControll();
 
     unset($_SESSION["loginAttempt"]);
     unset($_SESSION["logincaptcha"]);
@@ -42,6 +32,19 @@ class pageLoader {
   public function goToPage($url){
     header('Location: '.$url);
     exit;
+  }
+  public function pageControll(){
+    if (!isset($_SESSION["signedIn"])) {
+      $_SESSION["perms"]["pages"] = array("login", "cookies", "test", "bajs", "kuk", "whaapz");
+      $_SESSION["perms"]["perms"] = array("login");
+      if (!in_array($this->page, $_SESSION["perms"]["pages"])) {
+        $this->goToPage("?page=".$_SESSION["perms"]["pages"][0]);
+      }
+    }else if (isset($_SESSION["signedIn"])){
+      if (!in_array($this->page, $_SESSION)) {
+        $this->goToPage("?page=".$_SESSION["perms"]["pages"][0]);
+      }
+    }
   }
 }
 ?>
