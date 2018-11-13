d423532ab12ec5828669147016876247815a7774
mrboppe <33065458+mrboppe@users.noreply.github.com>
2018-11-13 20:47:09 +0100

    Update Backend/index.php

--- Backend/index.php
+++ Backend/index.php
@@ -21,7 +21,7 @@
     <link rel="stylesheet" href="css/master.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
-    <title><?php echo ucfirst($loader->page); ?></title>
+    <title><?php echo $company->companyName." - ".ucfirst($loader->page); ?></title>
   </head>
   <body>
     <?php
