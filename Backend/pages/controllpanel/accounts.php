5fe744d3e78105e2bf0c09974e30e6ceacfad81f
mrboppe <33065458+mrboppe@users.noreply.github.com>
2018-12-04 20:49:10 +0100

    css

--- Backend/pages/controllpanel/accounts.php
+++ Backend/pages/controllpanel/accounts.php
@@ -5,6 +5,10 @@
     <div id="headerspace">
       <a class="headerbutton" href="?page=accounts&createuser"><i class="fas fa-plus"></i> Create Account</a>
       <a class="headerbutton" href="?page=accounts"><i class="fas fa-list-ol"></i> Account list</a>
+      <?php if (isset($_REQUEST["id"])): ?>
+        <a class="headerbutton" href="" id="deleteuser"><i class="fas fa-trash-alt"></i> Delete account</a>
+        <a class="headerbutton" href="" id="edituser"><i class="fas fa-user-edit"></i> Edit Account</a>
+      <?php endif; ?>
     </div>
     <section>
       <?php
--- Backend/pages/controllpanel/accounts/create.php
+++ Backend/pages/controllpanel/accounts/create.php
@@ -1,3 +1,24 @@
-<form id="adduserForm">
-  
+<script type="text/javascript">
+$(document).ready(function () {
+  function readURL(input) {
+
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function(e) {
+        $('#accountImg').attr('src', e.target.result);
+      }
+
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  $("#imgInput").change(function() {
+    readURL(this);
+  });
+});
+
+</script>
+<form id="createAccount">
+  <input type='file' id="imgInput">
+  <img id="accountImg" src="#" alt="your image">
 </form>
