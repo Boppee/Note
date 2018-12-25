be3da2d8d10b98636afb84921e9f450d7fbdad35
Boppee <44766060+Boppee@users.noreply.github.com>
2018-12-25 13:57:10 +0100

    Perms

--- Backend/pages/controllpanel/accounts/account.php
+++ Backend/pages/controllpanel/accounts/account.php
@@ -1,3 +1,4 @@
+<title><?php echo $company->companyName." - ".ucfirst($loader->page)." - ".$_REQUEST["id"]; ?></title>
 <script type="text/javascript">
   var uid = "<?php echo $_REQUEST["id"]; ?>";
 </script>
@@ -89,4 +90,82 @@
   </div>
   </div>
 </section>
-<title><?php echo $company->companyName." - ".ucfirst($loader->page)." - ".$_REQUEST["id"]; ?></title>
+<section id="perms" class="Ypadding">
+  <div class="inner">
+    <div id="permdivs">
+
+      <div class="perms" id="accounts">
+        <form class="permform" id="accountsform">
+          <h1>Accounts</h1>
+          <div class="permlist">
+            <ul>
+              <li><input type="checkbox" name="all" value=""> All</li>
+              <li><input type="checkbox" name="list" value=""> List</li>
+              <li><input type="checkbox" name="respwd" value=""> Reset password</li>
+              <li><input type="checkbox" name="create" value=""> Create</li>
+              <li><input type="checkbox" name="mod" value=""> Modify</li>
+              <li><input type="checkbox" name="delete" value=""> Delete</li>
+            </ul>
+          </div>
+        </form>
+      </div>
+
+      <div class="perms" id="orders">
+        <form class="permform" id="ordersform">
+          <h1>Orders</h1>
+          <div class="permlist">
+            <ul>
+              <li><input type="checkbox" name="all" value=""> All</li>
+              <li><input type="checkbox" name="list" value=""> List</li>
+              <li><input type="checkbox" name="create" value=""> Create</li>
+              <li><input type="checkbox" name="mod" value=""> Modify</li>
+              <li><input type="checkbox" name="delete" value=""> Delete</li>
+            </ul>
+          </div>
+        </form>
+      </div>
+
+      <div class="perms" id="products">
+        <form class="permform" id="productsform">
+          <h1>Products</h1>
+          <div class="permlist">
+            <ul>
+              <li><input type="checkbox" name="all" value=""> All</li>
+              <li><input type="checkbox" name="list" value=""> List</li>
+              <li><input type="checkbox" name="create" value=""> Create</li>
+              <li><input type="checkbox" name="mod" value=""> Modify</li>
+              <li><input type="checkbox" name="delete" value=""> Delete</li>
+            </ul>
+          </div>
+        </form>
+      </div>
+
+      <div class="perms" id="categories">
+        <form class="permform" id="categoriesform">
+          <h1>Categories</h1>
+          <div class="permlist">
+            <ul>
+              <li><input type="checkbox" name="all" value=""> All</li>
+              <li><input type="checkbox" name="list" value=""> List</li>
+              <li><input type="checkbox" name="create" value=""> Create</li>
+              <li><input type="checkbox" name="mod" value=""> Modify</li>
+              <li><input type="checkbox" name="delete" value=""> Delete</li>
+            </ul>
+          </div>
+        </form>
+      </div>
+
+      <div class="perms" id="statistics">
+        <form class="permform" id="statisticsform">
+          <h1>Statistics</h1>
+          <div class="permlist">
+            <ul>
+              <li><input type="checkbox" name="list" value=""> List</li>
+            </ul>
+          </div>
+        </form>
+      </div>
+
+    </div>
+  </div>
+</section>
