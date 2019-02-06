7ead0103582b6002f87421e191e01fb982ab99ed
Boppee <44766060+Boppee@users.noreply.github.com>
2019-02-06 09:40:46 +0100

    fix

--- Backend/pages/page/login.php
+++ Backend/pages/page/login.php
@@ -12,47 +12,6 @@ $salt = new salt();
 <main>
 
   <nav>
-    <p class="text">Welcome to <?php echo $company->companyName; ?></p>
-    <ul>
-      <li>
-        <a class="text" href="#">Store</a>
-      </li>
-      <li>
-        <a class="text" href="#">Forgot password?</a>
-      </li>
-    </ul>
-    <i class="ico fas fa-caret-right"></i>
-  </nav>
-
-  <section>
-    <button type="button" name="button">Sign in</button>
-  </section>
-
-  <aside class="">
-    <div class="padder">
-      <header>
-        <h1>Sign in</h1>
-      </header>
-
-      <form id="capatcha">
-        <div class="g-recaptcha" data-sitekey="<?php echo $company->reCaptchaSiteKey; ?>" data-callback="sendRecaptcha"></div>
-      </form>
-      <form id="login">
-        <input type="text" id="uid" class="borderinput" placeholder="Username">
-        <input type="password" id="pwd" class="borderinput" placeholder="Password">
-        <input type="submit" id="sumbitlogin" value="Sign in">
-      </form>
-      <div class="loding">
-        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
-      </div>
-      <form id="email">
-        <input type="password" id="code" value="" placeholder="Email verification">
-        <input type="submit" name="" value="Sign in" id="sumbitcode">
-      </form>
-
-      <input type="hidden" id="salt" value="<?php $saltName = "login"; echo $salt->generatSalt($saltName);?>">
-
-    </div>
     <span><?php echo $company->companyName; ?> <i id="navarrow" class="fas fa-caret-right"></i></span>
     <ul id="navlist">
       <li><a href="">Store</a></li>
