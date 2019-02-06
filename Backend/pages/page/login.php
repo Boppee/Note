b7f70bdc1c78b035d8e43bf8998b36d5477761d2
Boppee <44766060+Boppee@users.noreply.github.com>
2019-02-06 09:29:17 +0100

    a

--- Backend/pages/page/login.php
+++ Backend/pages/page/login.php
@@ -9,33 +9,50 @@ $salt = new salt();
 </script>
 <script src="script/pages/login/login.js" charset="utf-8"></script>
 <link rel="stylesheet" href="css/page/login.css">
-<div id="outer">
-  <div id="inner">
-    <div id="formdiv">
-      <p>Sign in</p>
-      <div id="centerlogin">
-        <form id="capatcha">
-          <div class="g-recaptcha" data-sitekey="<?php echo $company->reCaptchaSiteKey; ?>" data-callback="sendRecaptcha"></div>
-        </form>
-        <form id="login">
-          <input type="text" id="uid" class="borderinput" placeholder="Username">
-          <input type="password" id="pwd" class="borderinput" placeholder="Password">
-          <input type="submit" id="sumbitlogin" value="Sign in">
-        </form>
-        <div class="loadImg">
-          <img src="img/load.gif" id="loadImg">
-        </div>
-        <form id="email">
-          <input type="password" id="code" value="" placeholder="Email verification">
-          <input type="submit" name="" value="Sign in" id="sumbitcode">
-        </form>
-        <input type="hidden" id="salt" value="<?php $saltName = "login"; echo $salt->generatSalt($saltName);?>">
+<main>
+
+  <nav>
+    <p class="text">Welcome to <?php echo $company->companyName; ?></p>
+    <ul>
+      <li>
+        <a class="text" href="#">Store</a>
+      </li>
+      <li>
+        <a class="text" href="#">Forgot password?</a>
+      </li>
+    </ul>
+    <i class="ico fas fa-caret-right"></i>
+  </nav>
+
+  <section>
+    <button type="button" name="button">Sign in</button>
+  </section>
+
+  <aside class="">
+    <div class="padder">
+      <header>
+        <h1>Sign in</h1>
+      </header>
+
+      <form id="capatcha">
+        <div class="g-recaptcha" data-sitekey="<?php echo $company->reCaptchaSiteKey; ?>" data-callback="sendRecaptcha"></div>
+      </form>
+      <form id="login">
+        <input type="text" id="uid" class="borderinput" placeholder="Username">
+        <input type="password" id="pwd" class="borderinput" placeholder="Password">
+        <input type="submit" id="sumbitlogin" value="Sign in">
+      </form>
+      <div class="loding">
+        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
       </div>
-      </div>
-    <div id="imgdiv">
-      <h1>
-        Welcome to <?php echo $company->companyName; ?>
-      </h1>
+      <form id="email">
+        <input type="password" id="code" value="" placeholder="Email verification">
+        <input type="submit" name="" value="Sign in" id="sumbitcode">
+      </form>
+
+      <input type="hidden" id="salt" value="<?php $saltName = "login"; echo $salt->generatSalt($saltName);?>">
+
     </div>
-  </div>
-</div>
+  </aside>
+
+</main>
